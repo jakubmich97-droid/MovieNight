971df2d2b9745be59c7f1cc0fee5bf9a04760f6b
jakubmich97-droid <jakubmich97@gmail.com>
2026-08-11 13:30:18 +0200

    Refresh draw fix cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="movienight-v5";
-const SHELL=["./","./index.html","./styles.css?v=5","./app.js?v=5","./config.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="movienight-v6";
+const SHELL=["./","./index.html","./styles.css?v=6","./app.js?v=6","./config.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET"||new URL(event.request.url).origin!==location.origin)return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html"))))});

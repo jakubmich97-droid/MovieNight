4e961871056825be9caa5c48b93335e8db52fe3a
jakubmich97-droid <jakubmich97@gmail.com>
2026-08-17 14:12:16 +0200

    Refresh PWA cache for v12

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="movienight-v11";
-const SHELL=["./","./index.html","./styles.css?v=10","./app.js?v=11","./config.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="movienight-v12";
+const SHELL=["./","./index.html","./styles.css?v=10","./app.js?v=12","./config.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET"||new URL(event.request.url).origin!==location.origin)return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html"))))});
